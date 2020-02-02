106728b0006cfcbdac98d143ec070b336b0fa18e
broholens <broholens@outlook.com>
2020-02-03 00:40:22 +0800

    全局使用axios

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,16 +3,29 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 import axios from 'axios'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import VueCookies from 'vue-cookies'
+import Vuex from 'vuex'
 
 Vue.use(ElementUI);
-Vue.use(axios);
+// Vue.use(axios);
 Vue.use(VueCookies)
 
-Vue.prototype.$http = axios;
+axios.defaults.withCredentials=true;
+axios.defaults.baseURL = 'http://localhost:5000/'
+axios.interceptors.request.use(function (config) {
+	let token = localStorage.getItem('token');
+  console.log(token);
+	if (token) {
+		config.headers.common['token'] = token;
+	}
+	return config;
+})
+
+Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false
 
@@ -20,6 +33,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
