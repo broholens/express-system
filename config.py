51af08b85f07834f1f54279dafd2e3b03f3c7010
broholens <broholens@github.com>
2020-02-04 19:21:03 +0800

    设置expire_time为1day

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 DB_NAME = 'express'
 EXPRESS_PRICE_HEADER = ['from_', 'to_', 'name', 'price_formula', 'remarks']
 UPLOAD_FOLDER = '/tmp'
-EXPIRE_TIME = 60 * 60
+EXPIRE_TIME = 60 * 60 * 24
 
 INIT_DB_SQL = f"""CREATE DATABASE IF NOT EXISTS {DB_NAME};
 USE {DB_NAME};
